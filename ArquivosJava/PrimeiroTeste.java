4d9f9f6e88ebafab041e5443047a8d762b19716d
Jonasdavi <jonas.davi@escolar.ifrn.edu.br>
2024-10-12 15:07:08 -0300

    classe pessoa criada

--- ArquivosJava/PrimeiroTeste.java
+++ ArquivosJava/PrimeiroTeste.java
@@ -1,5 +1,7 @@
 public class PrimeiroTeste{
     public static void main(String[] args) {
         System.out.println("Testando pela primeira vez o git hub!");
+
+        System.out.println("nao.");
     }
 }

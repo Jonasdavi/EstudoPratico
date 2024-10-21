7f7d9231845c655f1b763ccf163f06846893ae3b
Jonasdavi <jonas.davi@escolar.ifrn.edu.br>
2024-10-20 23:58:17 -0300

    reiniciando

--- ArquivosJava/PrimeiroTeste.java
+++
@@ -1,9 +0,0 @@
-public class PrimeiroTeste{
-    public static void main(String[] args) {
-        System.out.println("Testando pela primeira vez o git hub!");
-
-        System.out.println("atualizando codigo principal");
-        System.out.println("nao.");
-        System.out.println();
-    }
-}

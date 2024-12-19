12dd3133fad367a0f5714d041d06900ace3b0013
Jonasdavi <jonas.davi@escolar.ifrn.edu.br>
2024-12-19 11:52:05 -0300

    cont

--- bingo/src/App.java
+++ bingo/src/App.java
@@ -2,7 +2,27 @@ import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
         Cartela c= new Cartela();
+        Cartela c2=new Cartela();
 
-        c.criarCartela();
+        c.sortearNumsCartela();
+        c2.sortearNumsCartela();
+        c.exibirCartela();
+        System.out.println();
+        c2.exibirCartela();
+
+        System.out.println(c2.equals(c));
+
+        while(true){
+            if(c.equals(c2)){
+                System.out.println("igual");
+                break;
+            }
+            else{
+                System.out.println("nao igual");
+                c2.sortearNumsCartela();
+                continue;
+            }
+        }
+        
     }
 }
--- bingo/src/Cartela.java
+++ bingo/src/Cartela.java
@@ -3,24 +3,33 @@ import java.util.Arrays;
 
 public class Cartela{
     private int cartela[][];
-    private int[] coluna0, coluna1, coluna2, coluna3, coluna4; //variaveis para verificar numeros
+    private Integer[][] colunas; // para certificar que as colunas nao tenham numeros repetidos (Integer porque vou usar o metodo asList da classe Arrays que aceita apenas objetos)
 
     private final int TAMANHOLC=5; //tamanho da quantidade de linhas e colunas da cartela
+    private final int LUGARFREE=0;
 
     private Random rand= new Random();
 
     public Cartela(){
         //inicializando variaveis:
         cartela= new int[TAMANHOLC][TAMANHOLC];
-        
+        colunas= new Integer[TAMANHOLC][TAMANHOLC];
 
 
     }
 
-    public void criarCartela(){
-        //percorrendo o laço de forma que as arrays da matrizes sejam as colunas
+
+    public void sortearNumsCartela(){
+        //zerar numeros anteriores das colunas (para nao quebrar a verificaçao dos numeros repetidos)
         for(int c=0; c<TAMANHOLC; c++){
-            for(int l=0; l<TAMANHOLC; l++){
+            for(int n=0; n<TAMANHOLC; n++){
+                colunas[c][n]=0;
+            }
+        }
+
+        //percorrendo o laço de forma que as arrays da matrizes sejam as colunas
+        for(int l=0; l<TAMANHOLC; l++){
+            for(int c=0; c<TAMANHOLC; c++){
                 //para cada coluna, há um intervale de numero diferente
 
                 //coluna 0 (numeros possiveis: 1-15)
@@ -31,22 +40,57 @@ public class Cartela{
 
                 int numSorteado;
                 do{
-                    numSorteado=(rand.nextInt(15)+1) + 15*c; //ajusta o intervalo de numeros possiveis de ser sorteado de acordo com a coluna
-                }while(Arrays.asList(cartela[c]).contains(numSorteado)); //verifica se a coluna ja tem o numero sorteado:
+                    numSorteado=(rand.nextInt(6)+1) + 15*c; //ajusta o intervalo de numeros possiveis de ser sorteado de acordo com a coluna
+                }while(Arrays.asList(colunas[c]).contains(numSorteado)); //verifica se a coluna ja tem o numero sorteado:
 
-                cartela[c][l]=  numSorteado;
-                
-                
-                
+                colunas[c][l]=numSorteado; //adicionando o numero sorteado a sua respectiva coluna
+
+                cartela[l][c]=  numSorteado; //adicionando numero a cartela
             }
         }
+        //retirando o numero do meio da cartela por ele ser "free"
+        cartela[2][2]= LUGARFREE;
+        colunas[2][2]= LUGARFREE;
+
         
-        for(int c=0; c<TAMANHOLC; c++){
-            for(int l=0; l<TAMANHOLC; l++){
-                System.out.print(cartela[l][c]+" ");
-            }
-            System.out.println();
-        }
+        
+        
     }
 
+    public void exibirCartela(){
+        for(int l=0; l<TAMANHOLC; l++){
+            for(int c=0; c<TAMANHOLC; c++){
+                System.out.print(cartela[l][c]+ (cartela[l][c]<=9?"   ":"  "));
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+    }
+
+
+    @Override
+    public boolean equals(Object o){
+        Integer[][] colunas= ((Cartela) o).getColunas();
+
+        for(int col=0; col<TAMANHOLC; col++){
+            for(int n=0; n<TAMANHOLC; n++){
+                if(Arrays.asList(colunas[col]).contains(this.colunas[col][n])){
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public int[][] getCartela(){
+        return cartela;
+    }
+    public Integer[][] getColunas(){
+        return colunas;
+    }
 }
